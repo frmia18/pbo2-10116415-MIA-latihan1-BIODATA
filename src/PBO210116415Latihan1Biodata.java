e3f41ea1e9d409caeebb34e9f59b01576ee61656
frmia <frmiafm@gmail.com>
2020-10-21 00:07:19 +0700

    first commit

---
+++ src/PBO210116415Latihan1Biodata.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Frda
+ */
+public class PBO210116415Latihan1Biodata {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        System.out.println("Biodata" + "\n" + "--------------------------");
+        System.out.println("Nama\t\t: Mia Aisyah Farida");
+        System.out.println("Alamat\t\t: Jl. kha Badru Zaman");
+        System.out.println("Tempat Lahir\t: Garut");
+        System.out.println("Tanggal lahir\t: 18 Oktober 1997");
+        System.out.println("-----------------------------------");
+        System.out.println("Hobi\t\t: Traveling, Baca Buku");
+        System.out.println("MaKes\t\t: Bakso");
+        System.out.println("MiKes\t\t: Jus jeruk");
+    }
+    
+}
